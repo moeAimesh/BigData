5b27657c945213e23932c4a817fb08f6f322d74d
MoeAimesh <mohamad.aimesh.cgn@gmail.com>
2025-07-28 17:01:24 +0200

    Improve histogram: compact 3x32 RGB bins with numpy

--- src/features/color_vec.py
+++ src/features/color_vec.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 def calc_histogram(image):
-    hist = image.histogram()
-    hist = np.array(hist) / sum(hist)
-    return hist
+    arr = np.array(image.convert("RGB")).reshape(-1, 3)
+    hist_r, _ = np.histogram(arr[:, 0], bins=32, range=(0, 256), density=True)
+    hist_g, _ = np.histogram(arr[:, 1], bins=32, range=(0, 256), density=True)
+    hist_b, _ = np.histogram(arr[:, 2], bins=32, range=(0, 256), density=True)
+    return np.concatenate([hist_r, hist_g, hist_b])
