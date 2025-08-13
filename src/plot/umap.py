83d081da122efed0b6113b1676f2cebab24b57b9
AlJasmin <jasminalkarawani@outlook.de>
2025-08-13 14:16:08 +0200

    Verbindung zu SQlite Datenbank hergestellt

--- src/plot/umap.py
+++ src/plot/umap.py
@@ -1,3 +1,6 @@
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# === Verbindung öffnen (Pfad anpassen) ===
+conn = sqlite3.connect(r"Z:\CODING\UNI\BIG_DATA\data\database.db")
