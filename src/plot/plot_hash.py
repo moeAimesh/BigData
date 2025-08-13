56305e9e336c04effa2a7b6a95d25a6993307ace
AlJasmin <jasminalkarawani@outlook.de>
2025-08-13 14:21:01 +0200

    Imports und Konstanten

---
+++ src/plot/plot_hash.py
@@ -0,0 +1,7 @@
+import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
+
+# === Konstanten ===
+DB_PATH = r"Z:\CODING\UNI\BIG_DATA\data\database.db"
+TABLE_NAME = "image_features_part_6"
